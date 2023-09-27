a9bf5e9b5a55adc31ee16392d1b02d8017ee89a3
playbooksolitude <slatopr@gmail.com>
2023-09-27 14:30:08 +0900

    .

--- chapter/09/23-0927 stat_summary.R
+++ chapter/09/23-0927 stat_summary.R
@@ -54,4 +54,32 @@ bar + stat_summary(fun.data = median_hilow,
 
 
 #
-geom_errorbar()
+install.packages("nycflights13")
+library(nycflights13)
+flights |> count(origin, dest) -> fligts_1origin
+fligts_1origin |> group_by(origin) |> 
+  summarise(mean = mean(n), 
+            sd = sd(n)) 
+  #   origin  mean   sd
+  # 1  EWR    1405  1470
+  # 2  JFK    1590  1964
+  # 3  LGA    1539  2163
+
+#
+fligts_1origin |> ggplot(aes(origin, n)) -> fligts_2
+fligts_2 + 
+  stat_summary(geom = "bar", fun.y = "mean", 
+               fill = "white", color = "black") +
+  stat_summary(geom = "errorbar", 
+               fun.data = mean_cl_boot, width = .2) +
+  scale_y_continuous(breaks = c(1200, 1400, 1600))
+  
+#
+fligts_1origin |> summary()
+
+
+
+
+
+
+
