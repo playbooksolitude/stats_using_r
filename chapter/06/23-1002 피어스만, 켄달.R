c31cb7a89aaf11f6192630adf04999866ed1529a
playbooksolitude <slatopr@gmail.com>
2023-10-04 00:23:04 +0900

    .

---
+++ chapter/06/23-1002 피어스만, 켄달.R
@@ -0,0 +1,80 @@
+#23-1003 thus 23:08
+
+#
+library(tidyverse)
+library(car)
+
+#
+read_delim("./dataset/06/Exam Anxiety.dat") -> examdata
+
+#
+examdata
+  #Revise  복습   #연속형-구간
+  #Exam    성적   #범주형-순서
+  #Anxiety 불안   #연속형-구간
+
+#상관계수
+# cor.test()에 켄달 상관관계를 적용하면? 
+
+
+#Exam ~ Anxiety
+examdata$Exam |> hist()
+examdata$Anxiety |> hist()
+  #
+examdata$Exam |>qqPlot()
+examdata$Anxiety |> qqPlot()
+  #
+shapiro.test(examdata$Exam)    #정규성 미준수
+shapiro.test(examdata$Anxiety) #정규성 미준수
+
+  #Exam ~ Anxiety 는 비모수 상관계수를 구해야한다?
+cor.test(examdata$Exam, examdata$Anxiety, method = "pearson")
+cor.test(examdata$Exam, examdata$Anxiety, method = "spearman")
+cor.test(examdata$Exam, examdata$Anxiety, method = "kendall")
+
+lm(examdata$Exam ~ examdata$Anxiety)  # -0.665
+
+
+##Revise ~ Anxiety #비모수검정
+
+
+examdata$Revise
+examdata$Revise |> hist()
+shapiro.test(examdata$Revise)
+examdata$Revise |> qqPlot()
+
+cor.test(examdata$Revise, examdata$Anxiety, method = "pearson")
+cor.test(examdata$Revise, examdata$Anxiety, method = "spearman")
+cor.test(examdata$Revise, examdata$Anxiety, method = "kendall")
+
+examdata$Revise |> table()
+examdata$Anxiety |> table()
+
+
+# -------------------------------------------
+read_delim("./dataset/06/The Biggest Liar.dat", 
+           col_names = T) -> liardata
+
+liardata |> names()
+
+shapiro.test(liardata$Creativity) #정규성 o #연속
+liardata$Creativity |> hist()
+liardata$Creativity |> qqPlot()
+
+shapiro.test(liardata$Position)   #정규성 x #순서
+liardata$Position |> hist()
+liardata$Position |> qqPlot()
+
+cor.test(liardata$Creativity, liardata$Position, 
+         method = "spearman")
+
+#순서를 바꿀 경우 값은 같다
+cor.test(liardata$Position, liardata$Creativity,
+         method = "spearman")   #p-value 0.00172
+
+#단측검정
+cor.test(liardata$Position, liardata$Creativity,
+         method = "spearman", 
+         alternative = "less") #p-value 0.00086
+
+
--- chapter/06/23-1002 회귀1.R
+++ chapter/06/23-1002 회귀1.R
@@ -8,8 +8,15 @@ read_delim("./dataset/06/Exam Anxiety.dat") -> examdata
 examdata |> glimpse()
 
 #pearson
+  #cor.test() 
+  #p-value, 신뢰구간까지 구해준다
 cor.test(examdata$Revise, examdata$Anxiety, 
          method = "pearson")
+
+  #cor.test() 단점: 3개 이상은 구할 수 없다
+cor.test(examdata$Revise, examdata$Anxiety, examdata$Exam,
+         method = "pearson")
+
 
 #regression
 lm(examdata$Revise ~ examdata$Anxiety)
