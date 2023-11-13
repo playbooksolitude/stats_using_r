b9512bc7f0f35ae4d876f0919335f6fdfc77d3e8
playbooksolitude <slatopr@gmail.com>
2023-11-13 12:19:03 +0900

    .

--- chapter/08/23-1112 로지스틱회귀1.R
+++ chapter/08/23-1112 로지스틱회귀1.R
@@ -27,16 +27,31 @@ temp2 |> rename(
 temp3 |> colnames()
 temp3 |> select(동승자석, 없음)
 
-#
+# MASS 라이브러리에는 select가 있다
 mpg
 mpg |> dplyr::select(model, displ)
-?select
+
 
 #
 library(car)
 install.packages("mlogit")
 library(mlogit)
 
-#
+#파일 불러오기
 read_delim("./dataset/08/eel.dat") -> eeldata
 eeldata
+
+#기저 범주 지정 #가변수 범주 부호화
+eeldata$Cured
+eeldata$Cured |> factor() -> eeldata$Cured
+contrasts(eeldata$Cured)
+
+relevel(eeldata$Cured, ref = "Not Cured") -> eeldata$Cured
+contrasts(eeldata$Cured)
+relevel(eeldata$Cured, ref = "Not Cured") -> eeldata$Cured
+
+eeldata
+eeldata$Intervention |> factor() -> eeldata$Intervention
+contrasts(eeldata$Intervention)
+relevel(eeldata$Intervention, 
+        ref = "No Treatment") -> eeldata$Intervention
